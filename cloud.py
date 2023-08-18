4710453cb97e302c420f898e29f9ac39f57999b8
André <52675847+Adrm00@users.noreply.github.com>
2023-08-18 15:35:22 -0300

    Add files via upload

--- cloud.py
+++ cloud.py
@@ -5,9 +5,7 @@ processing_power = 1.25
 
 def process(n):
     time.sleep(1/processing_power * n)
+    print('Processamento na nuvem realizado em %f segundos' %(1/processing_power * n))
 
-start_time = time.time()
 process(int(sys.argv[1]))
-end_time = time.time()
 
-print((end_time - start_time))
