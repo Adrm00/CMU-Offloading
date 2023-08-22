40f32ca4190f7a6f142fca727c90a918c54cf6da
André <andreribeiromiranda5000@gmail.com>
2023-08-22 01:22:24 -0300

    big changes

--- cloud.py
+++ cloud.py
@@ -3,8 +3,15 @@ import time
 
 processing_power = 1.25
 
+transmission_rate = 10 #bytes/segundo 
+
 def process(n):
+
+    time.sleep(n/transmission_rate)
+
     time.sleep(1/processing_power * n)
+    
+    print('Mensagem transmitida em %f segundos' %(n/transmission_rate))
     print('Processamento na nuvem realizado em %f segundos' %(1/processing_power * n))
 
 process(int(sys.argv[1]))
--- offloading.py
+++ offloading.py
@@ -1,8 +1,10 @@
 import time
 import paramiko
 
-processing_power = 1.00
+local_processing_power = 1.00
 
+cloud_processing_power = 1.25
+cloud_transmission_rate = 10 #bytes/segundo
 
 # Separar bem cada tempo de execucao (latencia, transmissao, processamento)
 
@@ -15,7 +17,7 @@ processing_power = 1.00
 ## Taxa de transmissao
 
 def measure_local_execution_time(n):
-    local_processing_time = 1
+    local_processing_time = 1/local_processing_power
 
     print("PROCESSAMENTO LOCAL")
     print("Tempo de processamento para n = %d: %f" %(n, (local_processing_time * n)))
@@ -24,20 +26,22 @@ def measure_local_execution_time(n):
     return(local_processing_time * n)
 
 def measure_cloud_execution_time(n):
-    cloud_processing_time = 0.8
+    cloud_processing_time = 1/cloud_processing_power
     
+    # Medir tempo de conexao
     start_time = time.time()
     cloud_process(0)
     end_time = time.time()
 
-    connection_time = end_time - start_time
+    cloud_connection_time = end_time - start_time
 
     print("PROCESSAMENTO REMOTO")
-    print("Tempo de conexao: %f" %connection_time)
+    print("Tempo de conexao: %f" %cloud_connection_time)
+    print("Tempo de transmissao: %f" %(n/cloud_transmission_rate))
     print("Tempo de processamento para n = %d: %f" %(n, (cloud_processing_time * n)))
-    print("Tempo total: %f\n" %(connection_time + (cloud_processing_time * n)))
+    print("Tempo total: %f\n" %(cloud_connection_time + (n/cloud_transmission_rate) + (cloud_processing_time * n)))
 
-    return(connection_time + (cloud_processing_time * n))
+    return(cloud_connection_time + (n/cloud_transmission_rate) + (cloud_processing_time * n))
 
 def calculate_offloading_need(n):
     if measure_cloud_execution_time(n) < measure_local_execution_time(n):
@@ -51,7 +55,7 @@ def cloud_process(n):
     private_key = paramiko.RSAKey.from_private_key_file('CMU-2023-1-keypair.pem')
 
     # Depois tem que automatizar pegar esse IP, usando boto3 (se o ID da instancia nao mudar)
-    ssh.connect(hostname='3.83.246.122', username='ubuntu', pkey=private_key)
+    ssh.connect(hostname='3.95.245.233', username='ubuntu', pkey=private_key)
 
     # Executar o comando na instância e obter a saída.
     stdin, stdout, stderr = ssh.exec_command('cd CMU-Offloading; python3 cloud.py %s' %n)
@@ -63,7 +67,7 @@ def cloud_process(n):
     return float(output)
 
 def local_process(n):
-    time.sleep(1/processing_power * n)
+    time.sleep(1/local_processing_power * n)
 
 def process(n):
     if calculate_offloading_need(n) == True:
@@ -73,7 +77,5 @@ def process(n):
         local_process(n)
         print("O processamento foi feito localmente.")
 
-print(measure_local_execution_time(3))
-print(measure_cloud_execution_time(3))
-
-process(3)
+print(measure_local_execution_time(300))
+print(measure_cloud_execution_time(300))
